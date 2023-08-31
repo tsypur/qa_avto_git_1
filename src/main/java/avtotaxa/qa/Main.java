40586a901d4d5680e21458de66c43e0dad1062d3
natalyakrupnova <krupnova88@mail.ru>
2023-08-31 15:45:52 +0400

    added getMassage() method

--- src/main/java/avtotaxa/qa/Main.java
+++ src/main/java/avtotaxa/qa/Main.java
@@ -2,6 +2,10 @@ package avtotaxa.qa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello automation");
+        System.out.println(getMassage());
+    }
+
+    private static String getMassage() {
+        return "Hello automation";
     }
 }
