8b328e9058f7be37f7ef594ee91bbd25644c30b8
Natalya <83732245+tsypur@users.noreply.github.com>
2023-08-31 16:09:28 +0400

    Update Main.java

--- src/main/java/avtotaxa/qa/Main.java
+++ src/main/java/avtotaxa/qa/Main.java
@@ -2,7 +2,7 @@ package avtotaxa.qa;
 
 public class Main {
 
-    private static String msg = ""Hello automation!!!!!"";
+    private static String msg = "Hello automation!";
     
     public static void main(String[] args) {
         System.out.println(msg);
